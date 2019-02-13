81bbef2acd3d2f322d449ad48a5776b69123f640
François Revol <revol@free.fr>
2019-02-13 03:22:27 +0100

    Fix lqdon.sh for the new funding campaign

--- lqdon.sh
+++ lqdon.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-url="https://soutien.laquadrature.net/"
+url="https://www.laquadrature.net/donner/"
 
 exec 2>/dev/null
 
-R="$(wget -O - "$url" | sed -n '/&nbsp;&euro;<\/em>/{s,.*<em>,,;s,&.*,,;p}' | tr '\n' ' ')"
-M="${R%% *}"
-T="${R#* }"
+R="$(wget -O - "$url" | sed -n '/^Soit .*, sachant que/{s/^Soit //;s/€,.*$//;s/[^0-9]//g;p};/^<p>Depuis/{s/.*objectif de //;s/€ de.*//;s/[^0-9]//g;p}' | tr '\n' ' ')"
+M="${R#* }"
+T="${R%% *}"
 P="$(($M * 100 / $T))"
 
 echo " π⃝⃞  $P%"
@@ -15,6 +15,6 @@ echo "---"
 
 echo "$M € / $T €"
 
-echo "Donner… | href=https://soutien.laquadrature.net/"
+echo "Donner… | href=https://don.laquadrature.net/"
 
 exit 0
