8e124ef178d2f62f1a6c7f5d6e83d0ed214d593c
François Revol <revol@free.fr>
2021-11-18 20:26:55 +0100

    Update lqdon for the current LQDN donation campaign

--- lqdon.sh
+++ lqdon.sh
@@ -4,10 +4,10 @@ url="https://www.laquadrature.net/donner/"
 
 exec 2>/dev/null
 
-R="$(wget -O - "$url" | sed -n '/^Soit .*, sachant que/{s/^Soit //;s/€,.*$//;s/[^0-9]//g;p};/^<p>Depuis/{s/.*objectif de //;s/€ de.*//;s/[^0-9]//g;p}' | tr '\n' ' ')"
-M="${R#* }"
-T="${R%% *}"
-P="$(($M * 100 / $T))"
+R="$(wget -O - "$url" | sed -n '/^<h3.*%<\/h3>$/{s/^<h3 .*">//;s,%</h3>$,,;s/[^0-9]//g;p};/réunir au moins/{s/.*réunir au moins //;s/€ supplémentaires.*//;s/[^0-9]//g;p}' | tr '\n' ' ')"
+T="${R#* }"
+P="${R%% *}"
+M="$(($P * $T / 100))"
 
 echo " π⃝⃞  $P%"
 
