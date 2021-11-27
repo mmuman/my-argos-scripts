ed439141f7477f62c4d31adff81aa75f17642e93
François Revol <revol@free.fr>
2021-11-28 00:43:21 +0100

    Update lqdon to avoid reading non-rounded figure

--- lqdon.sh
+++ lqdon.sh
@@ -4,7 +4,7 @@ url="https://www.laquadrature.net/donner/"
 
 exec 2>/dev/null
 
-R="$(wget -O - "$url" | sed -n '/^<h3.*%<\/h3>$/{s/^<h3 .*">//;s,%</h3>$,,;s/[^0-9]//g;p};/réunir au moins/{s/.*réunir au moins //;s/€ supplémentaires.*//;s/[^0-9]//g;p}' | tr '\n' ' ')"
+R="$(wget -O - "$url" | sed -n '/^<p><img.*barre.*svg/{s,^<p><img.*barre/,,;s,\.svg"></p>$,,;s/[^0-9]//g;p};/réunir au moins/{s/.*réunir au moins //;s/€ supplémentaires.*//;s/[^0-9]//g;p}' | tr '\n' ' ')"
 T="${R#* }"
 P="${R%% *}"
 M="$(($P * $T / 100))"
