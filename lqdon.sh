98db80bdbc326849563f7a34cc7a77aa46c31e4a
François Revol <revol@free.fr>
2022-11-18 20:28:20 +0100

    lqdon: update for current campaign website

--- lqdon.sh
+++ lqdon.sh
@@ -4,10 +4,11 @@ url="https://www.laquadrature.net/donner/"
 
 exec 2>/dev/null
 
-R="$(wget -O - "$url" | sed -n '/^<p><img.*barre.*svg/{s,^<p><img.*barre/,,;s,\.svg"></p>$,,;s/[^0-9]//g;p};/réunir au moins/{s/.*réunir au moins //;s/€ supplémentaires.*//;s/[^0-9]//g;p}' | tr '\n' ' ')"
+R="$(wget -O - "$url" | sed -n '/^<p><span class="pourcents">/{s,^<p><span class="pourcents">,,;s,%</span> des dons .*$,,;s/[^0-9]//g;p};/réunir au moins/{s/.*réunir au moins //;s/€ supplémentaires.*//;s/[^0-9]//g;p}' | tr '\n' ' ')"
 T="${R#* }"
 P="${R%% *}"
-M="$(($P * $T / 100))"
+M="$(($P * $T / 100 / 100))"
+P="$(("$P"/100))"
 
 # Some Unicode magic here:
 # CF 80 = U+03C0 GREEK SMALL LETTER PI
